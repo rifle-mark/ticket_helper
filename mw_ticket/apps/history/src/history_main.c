5655582c33d503a11fe112a361a5eb36a785a46c
rifle <rifle@makeDEV>
2014-10-01 13:59:44 +0800

    init project folders

---
+++ mw_ticket/apps/history/src/history_main.c
@@ -0,0 +1,8 @@
+/*
+ * history_main.c
+ *
+ *  Created on: Oct 1, 2014
+ *      Author: rifle
+ */
+
+
---
+++ mw_ticket/request/inc/mw_request.h
@@ -0,0 +1,13 @@
+/*
+ * mw_request.h
+ *
+ *  Created on: Oct 1, 2014
+ *      Author: rifle
+ */
+
+#ifndef MW_REQUEST_H_
+#define MW_REQUEST_H_
+
+
+
+#endif /* MW_REQUEST_H_ */
